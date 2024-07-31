56c5f81299faaae0656b5e65f6ecda3bcf771c8c
islom2020 <islomjonanvarjonov@gmail.com>
2024-07-31 23:12:38 +0500

    vscode committ

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 
-from webhook_moysklad import moysklad_api
+import webhook_moysklad.moysklad_api
 
 app = Flask(__name__)
 
@@ -14,8 +14,8 @@ def webhook():
         # Process the webhook data here
         print("Received webhook data:", data)
 
-        size = moysklad_api.get_customer_orders_size()
-        moysklad_api.update_customer_order_check_number_by_id(order_id, size)
+        size = webhook_moysklad.moysklad_api.get_customer_orders_size()
+        webhook_moysklad.moysklad_api.update_customer_order_check_number_by_id(order_id, size)
 
         # Respond with a success message
         return jsonify({"message": "Webhook received successfully"}), 200
@@ -25,3 +25,4 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+    #test comment
